6092a143d364235f49610946fa94503237a328e5
Pablo Caballero <91947629+Pablo-Caballero-Maria@users.noreply.github.com>
2024-06-15 01:38:21 +0200

    debug set to false

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
